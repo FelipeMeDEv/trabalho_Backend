67e49f914200e6f2ad6a28429df5a0a921d8d7b0
Felipe de Melo <felipemsoares22@gmailcom>
2025-11-28 22:07:21 -0300

    consertando YAML

--- trabalho_backend/app.js
+++ trabalho_backend/app.js
@@ -1,5 +1,6 @@
+require('dotenv').config(); 
 const express = require('express');
-const connectDB = require('./config/db');
+const connectDB = require('./src/config/db');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
@@ -14,14 +15,11 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 
-// Carregar Swagger YAML
-const swaggerDocument = YAML.load(path.join(__dirname, '../docs/swagger.yaml'));
+const swaggerDocument = YAML.load(path.join(__dirname, './docs/swagger.yaml'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Rotas
-app.use('/tarefas', require('./routes/tarefaRoutes'));
+app.use('/tarefas', require('./src/routes/tarefaRoutes'));
 
-// Rota de Auth (Mock para gerar token e poder testar)
 app.post('/auth/login', (req, res) => {
     const jwt = require('jsonwebtoken');
     const token = jwt.sign({ user: { id: '123' } }, process.env.JWT_SECRET, { expiresIn: '1h' });
